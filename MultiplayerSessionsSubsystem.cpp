190897b94644b1dae4f7e3763d0a336acf9d8e7a
Samuele <samusonoio24@gmail.com>
2024-11-21 02:22:34 +0100

    Update MultiplayerSessionsSubsystem.cpp

--- MultiplayerSessionsSubsystem.cpp
+++ MultiplayerSessionsSubsystem.cpp
@@ -3,6 +3,7 @@
 
 #include "MultiplayerSessionsSubsystem.h"
 #include "OnlineSubsystem.h"
+#include "OnlineSessionSettings.h"
 
 void PrintString(const FString& str)
 {
@@ -14,6 +15,11 @@ void PrintString(const FString& str)
 
 UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
 {
+	// PrintString("MSS Constructor");
+}
+
+void UMultiplayerSessionsSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
 	IOnlineSubsystem* OnlineSubsystem = IOnlineSubsystem::Get();
 
 	if (OnlineSubsystem)
@@ -22,20 +28,62 @@ UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
 		PrintString(SubSystemName);
 
 		SessionInterface = OnlineSubsystem->GetSessionInterface();
-		if (SessionInterface.IsValid()) 
+		if (SessionInterface.IsValid())
 		{
-			PrintString("Session interface is valid!");
+			SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UMultiplayerSessionsSubsystem::OnCreateSessionComplete);
 		}
 	}
-	// PrintString("MSS Constructor");
-}
-
-void UMultiplayerSessionsSubsystem::Initialize(FSubsystemCollectionBase& Collection)
-{
 	// PrintString("MSS Initialize");
 }
 
 void UMultiplayerSessionsSubsystem::Deinitialize()
 {
 	// UE_LOG(LogTemp, Warning, TEXT("MSS Deinitialize"));
+}
+
+
+void UMultiplayerSessionsSubsystem::CreateServer(FString ServerName)
+{
+	PrintString("CreateServer");
+
+	if (ServerName.IsEmpty())
+	{
+		PrintString("Server name cannot be empty!");
+		return;
+	}
+
+	FName MySessionName = FName("Co-op Adventure Session Name");
+
+	FOnlineSessionSettings SessionSettings;
+
+	SessionSettings.bAllowJoinInProgress = true;
+	SessionSettings.bIsDedicated = false;
+	SessionSettings.bShouldAdvertise = true;
+	SessionSettings.NumPublicConnections = 2;
+	SessionSettings.bUseLobbiesIfAvailable = true;
+	SessionSettings.bUsesPresence = true;
+	SessionSettings.bAllowJoinViaPresence = true;
+	bool IsLAN = false;
+	if (IOnlineSubsystem::Get()->GetSubsystemName() == "NULL")
+	{
+		IsLAN = true;
+	}
+	SessionSettings.bIsLANMatch = IsLAN;
+
+	SessionInterface->CreateSession(0, MySessionName, SessionSettings);
+}
+
+
+void UMultiplayerSessionsSubsystem::FindServer(FString ServerName)
+{
+	PrintString("Find Server");
+}
+
+void UMultiplayerSessionsSubsystem::OnCreateSessionComplete(FName SessionName, bool WasSuccessful)
+{
+	PrintString(FString::Printf(TEXT("OnCreateSessionComplete: %d"), WasSuccessful));
+	if (WasSuccessful)
+	{
+		GetWorld()->ServerTravel("/Game/ThirdPerson/Maps/ThirdPersonMap?listen");
+	}
 }
